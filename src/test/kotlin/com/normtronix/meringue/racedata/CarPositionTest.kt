b982c45560afab72c68e8eb547d9ad67ae78c251
sprintf <paul@normingtons.org>
2022-01-09 13:35:26 -0800

    unnittest fix

--- src/test/kotlin/com/normtronix/meringue/racedata/CarPositionTest.kt
+++ src/test/kotlin/com/normtronix/meringue/racedata/CarPositionTest.kt
@@ -11,6 +11,11 @@ internal class CarPositionTest{
         val car2 = CarPosition("183", "", "A")
         car2.carInFront = car1
         car1.carInFront = null
+        // while we haven't started then this returns null
+        assertEquals(null, car2.getCarInFront(PositionEnum.IN_CLASS))
+        assertEquals(null, car2.getCarInFront(PositionEnum.OVERALL))
+        car1.position = 1
+        car2.position = 2
         assertEquals(car1, car2.getCarInFront(PositionEnum.IN_CLASS))
         assertEquals(car1, car2.getCarInFront(PositionEnum.OVERALL))
         assertEquals(null, car1.getCarInFront(PositionEnum.IN_CLASS))

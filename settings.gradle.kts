c4799bc7766ca65ed83a0aae4f60687550e15dc5
sprintf <paul@normingtons.org>
2023-08-26 21:12:44 -0700

    upgrade to kotlin 1.8.22

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ include("lemon-pi-protos")
 
 pluginManagement {
     plugins {
-        kotlin("jvm") version "1.6.21"
-        kotlin("plugin.spring") version "1.6.21"
+        kotlin("jvm") version "1.8.22"
+        kotlin("plugin.spring") version "1.8.22"
     }
 }

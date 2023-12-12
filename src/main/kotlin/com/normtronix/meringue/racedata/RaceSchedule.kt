6edd2690125dec503bc721de5f70b28d5f583602
PaulN <paul@normingtons.org>
2023-12-11 21:23:09 -0800

    fixes server startup (hopefully)

--- src/main/kotlin/com/normtronix/meringue/racedata/RaceSchedule.kt
+++ src/main/kotlin/com/normtronix/meringue/racedata/RaceSchedule.kt
@@ -11,10 +11,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Component
-import java.io.BufferedReader
-import java.io.FileReader
 import java.time.temporal.ChronoUnit
 import java.util.*
 import java.util.stream.Collectors
